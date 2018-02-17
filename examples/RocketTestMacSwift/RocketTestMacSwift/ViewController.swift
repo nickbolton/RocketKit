072be81b47d0a4db6499746582175a9f07237187
nickbolton <bolton.nick@gmail.com>
2018-02-17 13:03:51 -0600

    text layouts…

--- examples/RocketTestMacSwift/RocketTestMacSwift/ViewController.swift
+++ examples/RocketTestMacSwift/RocketTestMacSwift/ViewController.swift
@@ -13,21 +13,21 @@ class ViewController: RootViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let component = LayoutProvider.shared.componentByIdentifier("9AF4A96E-8872-4C81-B2F9-5522214FD9D7") {
-            component.textDescriptor?.text = "What in the world is happening here??"
-            
-            let deadline = DispatchTime.now() + 3.0
-            
-            DispatchQueue.main.asyncAfter(deadline: deadline) {
-                component.textDescriptor?.textAttributes.textAlignment = .center
-                component.textDescriptor?.text = "Weeee\nYo!"
-                
-                if let view = LayoutProvider.shared.view(with: component.identifier) {
-                    view.component = component
-                    view.updateText()
-                }
-            }
-        }
+//        if let component = LayoutProvider.shared.componentByIdentifier("9AF4A96E-8872-4C81-B2F9-5522214FD9D7") {
+//            component.textDescriptor?.text = "What in the world is happening here??"
+//            
+//            let deadline = DispatchTime.now() + 3.0
+//            
+//            DispatchQueue.main.asyncAfter(deadline: deadline) {
+//                component.textDescriptor?.textAttributes.textAlignment = .center
+//                component.textDescriptor?.text = "Weeee\nYo!"
+//                
+//                if let view = LayoutProvider.shared.view(with: component.identifier) {
+//                    view.component = component
+//                    view.updateText()
+//                }
+//            }
+//        }
     }
 }
 
